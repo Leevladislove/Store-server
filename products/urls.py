eb603cdbd4af34744a323e17716aa7dedb0866f5
Lee Vlad <leevladislove@gmail.com>
2023-07-17 23:42:37 +0600

    store_2

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from products.views import products, basket_add, basket_remove
 
 urlpatterns = [
     path('', products, name='products'),
+    path('category/<int:category_id>/', products, name='category'),
     path('basket/add/<int:product_id>/', basket_add, name='basket_add'),
     path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
