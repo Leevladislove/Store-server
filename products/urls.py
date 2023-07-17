67335aeb61f538f3c488e32c4d891169f7d091fa
Lee Vlad <leevladislove@gmail.com>
2023-07-17 13:17:55 +0600

    store_1

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from products.views import products, basket_add, basket_remove
+
+urlpatterns = [
+    path('', products, name='products'),
+    path('basket/add/<int:product_id>/', basket_add, name='basket_add'),
+    path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
+]
